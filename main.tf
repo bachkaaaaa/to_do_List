4642c1593dad3c5229b6931e411def922c97e4c0
Bachka Med Amin <134604575+bachkaaaaa@users.noreply.github.com>
2024-10-27 21:19:56 +0100

    vpc name

--- main.tf
+++ main.tf
@@ -5,6 +5,9 @@ provider "aws" {
 
 resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
+ tags = {
+    Name = "MyVPC" 
+  }
 }
  #"Public" subnet creation
 resource "aws_subnet" "public_subnet" {
